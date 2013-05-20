6d1d70e1dcd4ff263a5d8564c9b22644d765dc76
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-05-20 13:42:01 -0600

    Cleaned up the code for creating a rendering context from the request, response and next triplet

---
+++ RequestContext.js
@@ -0,0 +1,94 @@
+var renderContext = require('raptor/render-context');
+var templating = require('raptor/templating');
+var raptor = require('raptor');
+var dataProviders = require('raptor/data-providers');
+var Context = require('raptor/render-context/Context');
+
+var RequestContext = define.Class(
+    {
+        superclass: Context
+    },
+    function(require, exports, module) {
+        
+        var oldRenderTemplate = Context.prototype.renderTemplate;
+
+        function RequestContext(request, response, next) {
+            // Use the response object as the output writer
+            RequestContext.superclass.constructor.call(this, response);
+
+            var attributes = this.getAttributes();
+            attributes.request = request;
+            attributes.response = response;
+            attributes.app = request.app;
+            attributes.next = next;
+        }
+
+        // Copy properties to our new prototype to keep the prototype chain short:
+        raptor.extend(RequestContext.prototype, Context.prototype);
+
+        RequestContext.prototype.createDataProviders = function() {
+            // This method is invoked by the context.dataProviders method to
+            // create the initial DataProviders object
+            var appDataProviders = require('./index').dataProviders(this.app);
+
+            // Create a new DataProviders object for this context that extends
+            // the application data providers
+            return dataProviders.create(appDataProviders);
+        };
+
+        RequestContext.prototype.renderTemplate = function(templateName, data) {
+            // The first "renderTemplate" call is special because it is used
+            // to render the page and we automatically close the connection
+            // when the rendering is complete. We restore the original 
+            // "renderTemplate" method after the first call to this
+            // renderTemplate function
+            this.renderTemplate = oldRenderTemplate;
+            var _this = this;
+
+            function onError(e) {
+                var wrappedError = raptor.createError(new Error('Call to context.renderTemplate("' + templateName + '", ...) failed: ' + e), e);
+                _this.next(wrappedError);
+            }
+
+            try
+            {
+                var promise = templating.renderAsync(templateName, data, this),
+                    response = this.response;
+
+                promise
+                    .then(
+                        function(output) {
+                            response.end();
+                        },
+                        onError);
+                return promise; 
+            }
+            catch(e) {
+                onError(e);
+            }
+        };
+
+        return RequestContext;
+    });
+
+Object.defineProperty(RequestContext.prototype, "request", {
+    get: function() { return this.getAttributes().request; },
+    set: function(request) { this.getAttributes().request = request; }
+});
+
+Object.defineProperty(RequestContext.prototype, "response", {
+    get: function() {return this.getAttributes().response; },
+    set: function(response) { this.getAttributes().response = response; }
+});
+
+Object.defineProperty(RequestContext.prototype, "app", {
+    get: function() { return this.getAttributes().app; },
+    set: function(app) { this.getAttributes().app = app; }
+});
+
+Object.defineProperty(RequestContext.prototype, "next", {
+    get: function() {return this.getAttributes().next; },
+    set: function(next) { this.getAttributes().next = next; }
+});
+
+module.exports = RequestContext;
--- index.js
+++ index.js
@@ -1,80 +1,30 @@
-var renderContext = require('raptor/render-context');
-var templating = require('raptor/templating');
 var ExpressResetter = require('./ExpressResetter');
 var raptor = require('raptor');
+var dataProviders = require('raptor/data-providers');
+var RequestContext = require('./RequestContext');
 
-function dataProviders(app, newProviders){
-    var dataProviders = app.__raptorDataProviders;
+function getAppDataProviders(app, newProviders){
+
+    // See if data providers are already associated with the app...
+    var appDataProviders = app.__raptorDataProviders;
     
-    if (!dataProviders) {
-        dataProviders = app.__raptorDataProviders = new require('raptor/data-providers').create();
+    if (!appDataProviders) {
+        // It not, create the app data providers
+        appDataProviders = app.__raptorDataProviders = dataProviders.create();
     }
 
+    // If new providers have been passed in, then reigster those providers
     if (arguments.length > 1) {
-        dataProviders.register.apply(dataProviders, arguments);    
+        appDataProviders.register.apply(appDataProviders, raptor.arrayFromArguments(arguments, 1));    
     }
 
-    return dataProviders;
+    return appDataProviders;
 };
 
-function context_createDataProviders() {
-    var appDataProviders = dataProviders(this.getAttributes().expressApp);
-    return require('raptor/data-providers').create(appDataProviders);
-}
-
-function context_renderTemplate(templateName, data) {
-    // The first "renderTemplate" call is special because it is used
-    // to render the page and we automatically close the connection
-    // when the rendering is complete. We restore the original 
-    // "renderTemplate" method after the first call to this
-    // renderTemplate function
-    this.renderTemplate = this._oldRenderTemplate;
-    var _this = this;
-
-    function onError(e) {
-        var wrappedError = raptor.createError(new Error('Call to context.renderTemplate("' + templateName + '", ...) failed: ' + e), e);
-        _this.getAttributes().expressNext(wrappedError);
-    }
-
-    try
-    {
-        var promise = templating.renderAsync(templateName, data, this),
-        res = this.getAttributes().response;
-
-        promise
-            .then(
-                function(output) {
-                    res.end();
-                },
-                onError);
-        return promise; 
-    }
-    catch(e) {
-        onError(e);
-    }
-};
-
-function context_getRequest() {
-    return this.getAttributes().request;
-}
-
-function context_getResponse() {
-    return this.getAttributes().response;
-}
 
 function raptorHandler(userHandler) {
     return function(req, res, next) {
-        var context = renderContext.createContext(res);
-        var attributes = context.getAttributes();
-        attributes.request = req;
-        attributes.response = res;
-        attributes.expressApp = req.app;
-        attributes.expressNext = next;
-        context.createDataProviders = context_createDataProviders;
-        context._oldRenderTemplate = context.renderTemplate;
-        context.renderTemplate = context_renderTemplate;
-        context.getRequest = context_getRequest;
-        context.getResponse = context_getResponse;
+        var context = new RequestContext(req, res, next);
         userHandler(context, req, res, next);
     }
 };
@@ -109,9 +59,11 @@ function addOptimizerRoutes(app, pageOptimizer, express) {
 }
 
 exports.handler = raptorHandler;
-exports.dataProviders = dataProviders;
+exports.dataProviders = getAppDataProviders;
 exports.createExpressResetter = createExpressResetter;
 exports.addOptimizerRoutes = addOptimizerRoutes;
+exports.RequestContext = RequestContext;
+
 exports.resetRoutes = function(app, express) {
     if (app._expressResetter) {
         app._expressResetter.reset();
